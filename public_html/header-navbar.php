8c4fe621881d801b27754306ca6f72ea777bace7
Halle Zucker <hallez@umich.edu>
2014-10-20 22:28:47 -0700

    Extracted inlined css into /static/css/application.css

--- public_html/header-navbar.php
+++ public_html/header-navbar.php
@@ -11,24 +11,7 @@ print <<< EOT
 	<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
-<style>
-html,
-body {
-	height: 100%;
-	/* The html and body elements cannot have any padding or margin. */
-}
-
-/* Wrapper for page content to push down footer */
-#wrap {
-	min-height: 100%;
-	height: auto;
-	/* Negative indent footer by its height */
-	margin: 0 auto -81px;
-	/* Pad bottom by footer height */
-	padding: 0 0 81px;
-}
-</style>
-
+	<link rel="stylesheet" href="/static/css/application.css" type="text/css" />
 </head>
 
 <body>
