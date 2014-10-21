d4e1ca16dec6d1b22ca035cf029fe72fdb5bba93
Halle Zucker <hallez@umich.edu>
2014-10-20 22:12:05 -0700

    Add footer.

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,3 +1,22 @@
-      </div> <!-- /.container page  -->
-  </body>
+
+		</div> <!-- /.container page  -->
+	</div> <!-- /.wrap -->
+		<div id="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<hr/>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-11">
+						<p class="text-muted">&copy; 2014 Halle R. Zucker</p>
+					</div>
+					<div class="col-xs-1">
+						<p class="text-muted"><a href="/">Home</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
--- public_html/header-navbar.php
+++ public_html/header-navbar.php
@@ -4,16 +4,35 @@ print <<< EOT
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Halle Zucker</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Halle Zucker</title>
 
-  <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<style>
+html,
+body {
+	height: 100%;
+	/* The html and body elements cannot have any padding or margin. */
+}
+
+/* Wrapper for page content to push down footer */
+#wrap {
+	min-height: 100%;
+	height: auto;
+	/* Negative indent footer by its height */
+	margin: 0 auto -81px;
+	/* Pad bottom by footer height */
+	padding: 0 0 81px;
+}
+</style>
 
 </head>
 
 <body>
-  <div class="container page">
+	<div id="wrap">
+		<div class="container page">
 EOT;
 ?>
