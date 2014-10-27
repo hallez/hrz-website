0e2cb3bf67aee1ace54d32051120306589b49a16
Halle Zucker <hallez@umich.edu>
2014-10-26 21:12:14 -0700

    Added prior research page.

--- public_html/cv/index.php
+++ public_html/cv/index.php
@@ -3,6 +3,10 @@
 <div class="row">
 	<div class="col-xs-10 col-xs-offset-2">
 		<h1>CV</h1>
+	</div>
+</div> <!-- class="row" -->
+<div class="row">
+	<div class="col-xs-10 col-xs-offset-2">
 		<p><a href="Zucker_CV.pdf">Current CV &mdash; pdf</a></p>
 	</div>
 </div> <!-- class="row" -->
--- public_html/prior-research/index.php
+++ public_html/prior-research/index.php
@@ -2,18 +2,19 @@
 
 <div class="row">
 	<div class="col-xs-10 col-xs-offset-2">
-		<h1>Current research</h1>
+		<h1>Prior research</h1>
 	</div>
 </div> <!-- class="row" -->
 <div class="row">
 	<div class="col-xs-10 col-xs-offset-2">
-		<p><a href="http://dml.ucdavis.edu/">Charan Ranganath's Dynamic Memory Lab</a></p>
+		<h4>Elizabeth A. Kensinger</h4>
+		<p><a href="https://www2.bc.edu/elizabeth-kensinger/">Cognitive and Affective Neuroscience Lab</a></p>
 	</div>
 </div> <!-- class="row" -->
 <div class="row">
 	<div class="col-xs-10 col-xs-offset-2">
-		<p><a href="../prior-research">Prior research</a></p>
+		<h4>Patricia A. Reuter-Lorenz</h4>
+		<p><a href="http://www.sitemaker.umich.edu/parl.lab/patricia.reuter-lorenz">Cognitive and Affective Neuropsychology Lab</a></p>
 	</div>
 </div> <!-- class="row" -->
-
 <?php require('../footer.php'); ?>
