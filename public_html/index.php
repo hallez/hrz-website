b723ee3b50f2c48bd9d5bb018cdc1fb0576ca4b3
Halle Zucker <hallez@umich.edu>
2014-11-02 11:58:54 -0800

    Added brain image to homepage.

--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,15 @@
 <?php require('header-navbar.php'); ?>
 
 <div class="row">
-	<div class="col-xs-8 col-xs-offset-2">
+	<div class="col-xs-2">
+		<img src="https://s.gravatar.com/avatar/ceb96a0515eadec4560913866b7e5d18?s=200" class="img-responsive" style="margin-top: 20px;">
+	</div>
+	<div class="col-xs-8">
 		<h1>Halle Zucker</h1>
 		<p class="bio-brief">PhD student interested in memory and all things hippocampal.</p>
 	</div>
 	<div class="col-xs-2">
-		<img src="https://s.gravatar.com/avatar/ceb96a0515eadec4560913866b7e5d18?s=200" class="img-responsive" alt="Responsive image" style="margin-top: 20px;">
+		<img src="/static/img/HRZ_HC.png" class="img-responsive" style="margin-top: 20px;">
 	</div>
 </div> <!-- class="row" -->
 <div class="row">
